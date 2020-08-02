3339cd2c9f55a59f0e0a1e2ed74f9901ec628c17
Your Name <you@example.com>
2020-08-01 21:04:32 -0700

    server is up on running

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const app = require('./app');
+
+require('dotenv').config()
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}...`)
+})
